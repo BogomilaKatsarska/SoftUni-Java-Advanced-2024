7a7d020641fd901279425303539c7df93edbbf21
Bogomila Katsarska <90954520+BogomilaKatsarska@users.noreply.github.com>
2024-10-23 14:39:35 +0300

    Add files via upload

---
+++ L06_PersonMain.java
@@ -0,0 +1,13 @@
+package JavaAdvanced;
+
+public class L06_PersonMain {
+    public static void main(String[] args) {
+        L06_Person person = new L06_Person();
+        person.setName("Bogomila");
+        person.setAge(29);
+        person.sayHello();
+
+    }
+    //static members - static methods or static fields
+
+}
